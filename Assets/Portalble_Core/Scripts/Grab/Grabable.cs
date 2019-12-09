5747ca1c28a37247ede62a19d8da5666c1dd4d1b
Jiaju Ma <jiaju_ma@brown.edu>
2019-12-09 16:10:36 -0500

    updated colliding behavior. Using capsule collider for cone for now

--- Assets/Portalble_Core/Scripts/Grab/Grabable.cs
+++ Assets/Portalble_Core/Scripts/Grab/Grabable.cs
@@ -202,7 +202,7 @@ namespace Portalble.Functions.Grab {
                 cd.isTrigger = false;
             Rigidbody rb = GetComponent<Rigidbody>();
             if (rb != null) {
-                rb.useGravity = true;
+                //rb.useGravity = true; //disabled for FoamAR
                 rb.velocity = Vector3.zero;
                 rb.angularVelocity = Vector3.zero;
                 rb.velocity = releaseVelocity * m_throwPower;

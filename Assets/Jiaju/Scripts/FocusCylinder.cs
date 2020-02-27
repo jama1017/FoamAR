41e219ab403a19618b9df4cc17459bd0b8bcf5eb
Jiaju Ma <jiaju_ma@brown.edu>
2020-02-27 17:27:29 -0500

    Configured focus cylinder prefab. Detecting colliding objects but do not trigger physics

---
+++ Assets/Jiaju/Scripts/FocusCylinder.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FocusCylinder : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("FOCUSED on object " + other.tag);
+
+        if (other.tag != "ARPlane") // ignore ARPlane prefab
+        {
+            other.gameObject.GetComponent<Renderer>().material.color = Color.yellow;
+        }
+        //material.color = Color.blue
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag != "ARPlane") // ignore ARPlane prefab
+        {
+            other.gameObject.GetComponent<Renderer>().material.color = Color.white;
+        }
+        //material.color = Color.blue
+    }
+}

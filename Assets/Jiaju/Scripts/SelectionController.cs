485bdc662ddc698ed06d4b3b99d06daeea4b9d4c
Jiaju Ma <jiaju_ma@brown.edu>
2020-02-27 14:45:42 -0500

    placing a cylinder to represent focus region. moving cylinder to the last grabbed focus point. Will working on scaling and collider of the cylinder

--- Assets/Jiaju/Scripts/SelectionController.cs
+++ Assets/Jiaju/Scripts/SelectionController.cs
@@ -23,6 +23,7 @@ public class SelectionController : PortalbleGeneralController
     // selection specific
     private bool m_isRecorded = false;
     private List<GameObject> m_markers = new List<GameObject>();
+    private List<Vector3> m_markers_screenPos = new List<Vector3>();
     private bool m_isMarkerDisplayed = false;
 
     // ------ IPs----------
@@ -32,7 +33,9 @@ public class SelectionController : PortalbleGeneralController
     public string websocketPort = "8765";
 
     // selection cylinder
-    private Transform m_cubeTest;
+    //private Transform m_cubeTest;
+    public Transform m_focusCylinderPrefab;
+    private Transform m_focusCylinder;
     private float m_v = 0f;
     private float m_h = 0f;
 
@@ -72,10 +75,12 @@ public class SelectionController : PortalbleGeneralController
         setupServer();
 
         //test. to be removed
-        m_cubeTest = Instantiate(placePrefab, m_FirstPersonCamera.transform.position + 0.2f * m_FirstPersonCamera.transform.forward, Quaternion.identity);
-        m_cubeTest.gameObject.GetComponent<Collider>().attachedRigidbody.useGravity = false;
+        //m_cubeTest = Instantiate(placePrefab, m_FirstPersonCamera.transform.position + 0.2f * m_FirstPersonCamera.transform.forward, Quaternion.identity);
+        //m_cubeTest.gameObject.GetComponent<Collider>().attachedRigidbody.useGravity = false;
 
-
+        m_focusCylinder = Instantiate(m_focusCylinderPrefab, m_FirstPersonCamera.transform.position + 0.2f * m_FirstPersonCamera.transform.forward, Quaternion.identity);
+        m_focusCylinder.gameObject.GetComponent<Collider>().attachedRigidbody.useGravity = false;
+        
     }
 
     private void setupServer()
@@ -106,34 +111,52 @@ public class SelectionController : PortalbleGeneralController
         //}
 
        
-        if (Input.touchCount > 0)
-        {
-            Vector3 touchPos = Input.GetTouch(0).position;
-            Vector3 center = new Vector3(Camera.main.pixelWidth / 2, Camera.main.pixelHeight / 2, 0);
+        //if (Input.touchCount > 0)
+        //{
+        //    Vector3 touchPos = Input.GetTouch(0).position;
+        //    Vector3 center = new Vector3(Camera.main.pixelWidth / 2, Camera.main.pixelHeight / 2, 0); // clean up
 
-            Vector2 distance_vec = touchPos - center;
+        //    Vector2 distance_vec = touchPos - center;
+
+        //    m_v = distance_vec.y / 10000f;
+        //    m_h = distance_vec.x / 10000f;
+
+        //    Debug.Log("SEM: m_v:" + m_v);
+        //    Debug.Log("SEM: m_h:" + m_h);
+
+        //    //Debug.Log("SEM touchPos: " + touchPos);
+        //    //Debug.Log("SEM touchPos world: " + Camera.main.ScreenToWorldPoint(touchPos));
+        //    //Debug.Log("SEM touchPos zero: " + Camera.main.ScreenToWorldPoint(center));
+
+        //    //Debug.Log("SEM touchPos distance: " + Vector3.Distance(Camera.main.ScreenToWorldPoint(touchPos), Camera.main.ScreenToWorldPoint(center)).ToString("F3"));
+        //    //Debug.Log(Camera.main.pixelHeight); // screen height in px
+        //    //Debug.Log(Camera.main.pixelWidth);  // screen width in px
+        //}
+
+        if (m_markers_screenPos.Count > 0) {
+            Vector3 center = new Vector3(Camera.main.pixelWidth / 2, Camera.main.pixelHeight / 2, 0);
+            Vector3 focusPos = m_markers_screenPos[m_markers_screenPos.Count - 1];
+
+            Vector2 distance_vec = focusPos - center;
 
             m_v = distance_vec.y / 10000f;
             m_h = distance_vec.x / 10000f;
 
-            Debug.Log("SEM: m_v:" + m_v);
-            Debug.Log("SEM: m_h:" + m_h);
-
-            //Debug.Log("SEM touchPos: " + touchPos);
-            //Debug.Log("SEM touchPos world: " + Camera.main.ScreenToWorldPoint(touchPos));
-            //Debug.Log("SEM touchPos zero: " + Camera.main.ScreenToWorldPoint(center));
-
-            //Debug.Log("SEM touchPos distance: " + Vector3.Distance(Camera.main.ScreenToWorldPoint(touchPos), Camera.main.ScreenToWorldPoint(center)).ToString("F3"));
-            //Debug.Log(Camera.main.pixelHeight); // screen height in px
-            //Debug.Log(Camera.main.pixelWidth);  // screen width in px
+            Debug.Log("SEM m_v: " + m_v);
+            Debug.Log("SEM m_h: " + m_h);
         }
 
         //Vector3 cameraOffset = screenToWorldSpace(m_canvas, m_touchPosTest);
 
-        //float fac = placePrefab.gameObject.GetComponent<Renderer>().bounds.size[0];
+        float fac = m_focusCylinderPrefab.gameObject.GetComponent<Renderer>().bounds.size[1];
 
-        m_cubeTest.position = m_FirstPersonCamera.transform.position + 0.5f * m_FirstPersonCamera.transform.forward + m_h * m_FirstPersonCamera.transform.right + m_v * m_FirstPersonCamera.transform.up;
-        m_cubeTest.LookAt(m_cubeTest.position - (m_FirstPersonCamera.transform.position - m_cubeTest.position));
+        m_focusCylinder.position = m_FirstPersonCamera.transform.position + fac * m_FirstPersonCamera.transform.forward + m_h * m_FirstPersonCamera.transform.right + m_v * m_FirstPersonCamera.transform.up;
+        m_focusCylinder.LookAt(m_focusCylinder.position - (m_FirstPersonCamera.transform.position - m_focusCylinder.position));
+        m_focusCylinder.Rotate(90.0f, 0.0f, 0.0f, Space.Self);
+
+        //setting scale
+        float initial_width = Camera.main.pixelWidth / (2 * 10000f); //change back to 2
+        m_focusCylinder.localScale = new Vector3(initial_width, m_focusCylinder.localScale[1], initial_width);
 
         //Vector2 newPos = worldToUISpace(m_canvas, m_FirstPersonCamera.transform.position);
         //Debug.Log("SELCAM: " + newPos);
@@ -194,6 +217,7 @@ public class SelectionController : PortalbleGeneralController
                 GameObject new_marker = Instantiate(prefab_marker, Vector3.zero, Quaternion.identity, m_canvas.transform);
                 new_marker.GetComponent<RectTransform>().anchoredPosition = newPos;
                 m_markers.Add(new_marker);
+                m_markers_screenPos.Add(worldToScreenSpace(grabPos));
                 new_marker.SetActive(m_isMarkerDisplayed);
                 
                 if (Jetfire.IsConnected2())

aba820c8b1606cb9d82d8ab5e7a6fa01028bb42e
Jiaju Ma <jiaju_ma@brown.edu>
2020-04-23 13:23:37 -0400

    bounding box cubes placement algo. moved cone pivot to center

--- Assets/Jiaju/Scripts/Modelable.cs
+++ Assets/Jiaju/Scripts/Modelable.cs
@@ -187,4 +187,27 @@ public class Modelable : MonoBehaviour
 
         return false;
     }
+
+
+    // debug script
+    void OnDrawGizmosSelected()
+    {
+        //Bounds curBound = transform.GetComponent<Renderer>().bounds;
+        Bounds curBound = transform.GetComponent<MeshFilter>().sharedMesh.bounds;
+
+        // Display the explosion radius when selected
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position, new Vector3(curBound.extents.magnitude, curBound.extents.magnitude, curBound.extents.magnitude));
+        Gizmos.DrawLine(curBound.max, curBound.min);
+        float offset = 0.05f;
+
+        // placement algorithms
+        Gizmos.DrawCube(transform.position + transform.up * (transform.localScale[1] * curBound.size.y / 2 + offset), new Vector3(0.03f, 0.03f, 0.03f));
+        Gizmos.DrawCube(transform.position - transform.up * (transform.localScale[1] * curBound.size.y / 2 + offset), new Vector3(0.03f, 0.03f, 0.03f));
+        Gizmos.DrawCube(transform.position + transform.right * (transform.localScale[0] * curBound.size.x / 2 + offset), new Vector3(0.03f, 0.03f, 0.03f));
+        Gizmos.DrawCube(transform.position - transform.right * (transform.localScale[0] * curBound.size.x / 2 + offset), new Vector3(0.03f, 0.03f, 0.03f));
+        Gizmos.DrawCube(transform.position + transform.forward * (transform.localScale[2] * curBound.size.z / 2 + offset), new Vector3(0.03f, 0.03f, 0.03f));
+        Gizmos.DrawCube(transform.position - transform.forward * (transform.localScale[2] * curBound.size.z / 2 + offset), new Vector3(0.03f, 0.03f, 0.03f));
+    }
+
 }
